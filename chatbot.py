93affff403ad807a13efa7e050e2ba07d8e2e93d
Komal Saini <sani9komal@gmail.com>
2026-06-16 22:34:49 +0530

    Initial commit - Basic Rule Based Chatbot

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+def chatbot():
+    print("Chatbot: Hello! Type 'bye' to end the chat.")
+
+    while True:
+        user = input("You: ").lower()
+
+        if user == "hello":
+            print("Chatbot: Hi!")
+        
+        elif user == "how are you":
+            print("Chatbot: I'm fine, thanks!")
+        
+        elif user == "bye":
+            print("Chatbot: Goodbye!")
+            break
+        
+        else:
+            print("Chatbot: Sorry, I don't understand that.")
+
+chatbot()  # Restart the chatbot for unrecognized input
